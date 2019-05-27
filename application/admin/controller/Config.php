c07a48acd53d4f5679a9c129795403a5d9f457ff
office <yongqiaoqu@163.com>
2019-05-27 17:19:22 +0800

    config配置展示修提交

--- application/admin/controller/Config.php
+++ application/admin/controller/Config.php
@@ -1,13 +1,14 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use think\Db;
 class Config extends Controller
 {
     public function lst()
     {
     	$config=model('Config');
     	$conres=$config->select();
-    	$this->assign('conres',$conres);
+    	$this->assign('conres',$conres);    	
        return $this->fetch();
     }
 
@@ -55,9 +56,51 @@ class Config extends Controller
     			$this->error('配置项删除失败，请重试');
     		}
     }
+
+
+
+
+
+
      public function options()
     
     {
+    	$config=model('Config');
+		$conres=$config->select();
+    	$this->assign('conres',$conres);
+    	if(request()->isPost()){
+    		$imgs=$_FILES;
+    		//halt($imgs);
+    		$data=$_POST;
+    		foreach ($data as $k => $v) {
+    			if(is_array($v)){
+    				$v=implode(',', $v);
+    			}
+    			Db::table('cms_config')->where('cf_ename',$k)->update(['cf_value'=>$v]);
+    		}
+    	
+    		
+    		foreach ($imgs as $k => $v) {
+    			//halt($v);
+    			if($v['name']!=''){
+    				$file = request()->file($k);
+    				//halt($file);
+    			$info = $file->move( './uploads');
+    			if($info){
+    				$value='/uploads/'.$info->getSaveName();
+    				 Db::table('cms_config')->where('cf_ename',$k)->update(['cf_value'=>$value]);
+    			}
+    			else{        
+          			echo $file->getError();
+    				}
+    			}
+    			
+    		}
+    		
+    		
+    	
+    	$this->success('配置修改成功');
+    	}
        return $this->fetch();
     }
 
---
+++ runtime/temp/2f25cec6d65a471b26a516c1b81f7c82.php
@@ -0,0 +1,346 @@
+<?php /*a:3:{s:54:"D:\wamp\www\cms\application\admin\view\config\lst.html";i:1558924631;s:55:"D:\wamp\www\cms\application\admin\view\common\head.html";i:1558588993;s:55:"D:\wamp\www\cms\application\admin\view\common\left.html";i:1558589964;}*/ ?>
+<!DOCTYPE html>
+<html><head>
+	    <meta charset="utf-8">
+    <title>配置列表-后台管理</title>
+
+    <meta name="description" content="Dashboard">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--Basic Styles-->
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/font-awesome.css" rel="stylesheet">
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/weather-icons.css" rel="stylesheet">
+
+    <!--Beyond styles-->
+    <link id="beyond-link" href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/beyond.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/demo.css" rel="stylesheet">
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/typicons.css" rel="stylesheet">
+    <link href="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/animate.css" rel="stylesheet">
+    
+</head>
+<body>
+	<!-- 头部 -->
+	
+	<!-- 头部 -->
+	<div class="navbar">
+    <div class="navbar-inner">
+        <div class="navbar-container">
+            <!-- Navbar Barnd -->
+            <div class="navbar-header pull-left">
+                <a href="#" class="navbar-brand" style="margin-top:10px; ">
+                   cms后台
+                </a>
+            </div>
+            <!-- /Navbar Barnd -->
+            <!-- Sidebar Collapse -->
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="collapse-icon fa fa-bars"></i>
+            </div>
+            <!-- /Sidebar Collapse -->
+            <!-- Account Area and Settings -->
+            <div class="navbar-header pull-right">
+                <div class="navbar-account">
+                    <ul class="account-area">
+                        <li>
+                            <a class="login-area dropdown-toggle" data-toggle="dropdown">
+
+                                <section>
+                                    <h2><span class="profile"><span>admin</span></span></h2>
+                                </section>
+                            </a>
+                            <!--Login Area Dropdown-->
+                            <ul class="pull-right dropdown-menu dropdown-arrow dropdown-login-area">
+                                <li class="username"><a>David Stevenson</a></li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/logout.html">
+                                            退出登录
+                                        </a>
+                                </li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/changePwd.html">
+                                            修改密码
+                                        </a>
+                                </li>
+                            </ul>
+                            <!--/Login Area Dropdown-->
+                        </li>
+                        <!-- /Account Area -->
+                        <!--Note: notice that setting div must start right after account area list.
+                            no space must be between these elements-->
+                        <!-- Settings -->
+                    </ul>
+                </div>
+            </div>
+            <!-- /Account Area and Settings -->
+        </div>
+    </div>
+</div>
+
+	<!-- /头部 -->
+	
+
+
+	<!-- /头部 -->
+	
+	<div class="main-container container-fluid">
+		<div class="page-container">
+			            <!-- Page Sidebar -->
+            
+			            <!-- Page Sidebar -->
+            <div class="page-sidebar" id="sidebar">
+                <!-- Page Sidebar Header-->
+                <div class="sidebar-header-wrapper">
+                    <input class="searchinput" type="text">
+                    <i class="searchicon fa fa-search"></i>
+                    <div class="searchhelper">Search Reports, Charts, Emails or Notifications</div>
+                </div>
+                <!-- /Page Sidebar Header -->
+                <!-- Sidebar Menu -->
+                <ul class="nav sidebar-menu">
+                    <!--Dashboard-->
+
+                                        <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                分类管理
+                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                            <ul class="submenu">
+                             <li>
+                                    <a href="<?php echo url('cate/lst'); ?>">
+                                    <span class="menu-text">
+                                       分类列表
+                                    </span>
+                                     <i class="menu-expand"></i>
+                                                    </a>
+                              </li>
+
+                              </ul>
+                                        </li>
+                                        <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                文档管理                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                                                <ul class="submenu">
+                                                        <li>
+                                <a href="<?php echo url('article/lst'); ?>">
+                                    <span class="menu-text">
+                                        文章列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                                                            </li>
+                            
+                        </ul>                            
+                                            </li>
+
+
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                留言管理
+                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('liuyan/lst'); ?>">
+                                    <span class="menu-text">
+                                       留言列表
+                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                小工具管理
+                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('widget/lst'); ?>">
+                                    <span class="menu-text">
+                                       小工具列表
+                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                友链管理
+                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('link/lst'); ?>">
+                                    <span class="menu-text">
+                                       友链列表
+                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </li>
+
+
+                                        <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+
+                            <span class="menu-text">
+                                系统                            </span>
+
+                            <i class="menu-expand"></i>
+                        </a>
+                                                <ul class="submenu">
+                                                        <li>
+                                <a href="<?php echo url('config/lst'); ?>">
+                                    <span class="menu-text">
+                                        配置列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                                                            </li>
+                                                        <li>
+                                <a href="<?php echo url('config/options'); ?>">
+                                    <span class="menu-text">
+                                        配置管理                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                                                            </li>
+
+                            
+                        </ul>                            
+                                            </li>                        
+                    
+                </ul>
+                <!-- /Sidebar Menu -->
+            </div>
+            <!-- /Page Sidebar -->
+
+            <!-- /Page Sidebar -->
+            <!-- Page Content -->
+            <div class="page-content">
+                <!-- Page Breadcrumb -->
+                <div class="page-breadcrumbs">
+                    <ul class="breadcrumb">
+                                        <li>
+                        <a href="#">系统</a>
+                    </li>
+                                        <li class="active">配置管理</li>
+                                        </ul>
+                </div>
+                <!-- /Page Breadcrumb -->
+
+                <!-- Page Body -->
+                <div class="page-body">
+<form action=""  method="post">
+<button type="button" tooltip="配置修改" class="btn btn-sm btn-azure btn-addon" onClick="javascript:window.location.href = 'add'"> <i class="fa fa-plus"></i> Add
+</button>
+   
+
+<div class="row">
+    <div class="col-lg-12 col-sm-12 col-xs-12">
+        <div class="widget">
+            <div class="widget-body">
+                <div class="flip-scroll">
+                    <table class="table table-bordered table-hover">
+                        <thead class="">
+                            <tr>
+                                <th class="text-center">ID</th>
+                                <th class="text-center">配置中文名称</th>
+                                <th class="text-center">配置英文名称</th>
+                                <th class="text-center">配置可选值</th>
+                                <th class="text-center">配置选中值</th>
+                                <th class="text-center">配置类型</th>
+                                <th class="text-center">配置所属项</th>
+                                <th class="text-center">操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php if(is_array($conres) || $conres instanceof \think\Collection || $conres instanceof \think\Paginator): $i = 0; $__LIST__ = $conres;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>
+                                                        <tr>
+                                <td align="center"><?php echo htmlentities($vo['cf_id']); ?></td>
+                                <td align="left"><?php echo htmlentities($vo['cf_cname']); ?></td>
+                                <td align="left"><?php echo htmlentities($vo['cf_ename']); ?></td>
+                                <td align="left"><?php echo htmlentities($vo['cf_values']); ?></td>
+                                <td align="left"><?php echo htmlentities($vo['cf_value']); ?></td>
+                                 <td align="center">
+                                     <?php switch($vo['dt_type']): case "1": ?>文本框<?php break; case "2": ?>文本域<?php break; case "3": ?>下拉菜单<?php break; case "4": ?>复选框<?php break; case "5": ?>单选框<?php break; case "6": ?>附件<?php break; default: ?>无
+                                     <?php endswitch; ?>
+                                 </td>
+                                 <td align="center">
+                                     <?php switch($vo['cf_type']): case "1": ?>基本配置<?php break; case "2": ?>联系方式<?php break; case "3": ?>SEO配置<?php break; ?>                                     }
+                                     <?php default: ?>无
+                                     <?php endswitch; ?>
+                                 </td>
+                                    
+                                <td align="center">
+                                    <a href="edit?cf_id=<?php echo htmlentities($vo['cf_id']); ?>" class="btn btn-primary btn-sm shiny">
+                                        <i class="fa fa-edit"></i> 编辑
+                                    </a>
+                                    <a href="#" onClick="warning('确实要删除吗', 'del?cf_id=<?php echo htmlentities($vo['cf_id']); ?>')" class="btn btn-danger btn-sm shiny">
+                                        <i class="fa fa-trash-o"></i> 删除
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; endif; else: echo "" ;endif; ?>
+
+
+                                                    </tbody>
+                    </table>
+                </div>
+                <div>
+                	                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
+                </div>
+                <!-- /Page Body -->
+            </div>
+            <!-- /Page Content -->
+		</div>	
+	</div>
+
+    <script src="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/jquery_002.js"></script>
+    <script src="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/bootstrap.js"></script>
+    <script src="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/jquery.js"></script>
+    <!--Beyond Scripts-->
+    <script src="<?php echo htmlentities(app('config')->get('__STATIC__')); ?>/admin/style/beyond.js"></script>
+    
+
+
+</body></html>
